e43c85bda8ed4902832280f9015d20afa7c08f20
Student <example@example.com>
2019-02-25 20:38:04 -0800

    Implemented the GetAll() public method

--- ModelTests/ItemTest.cs
+++ ModelTests/ItemTest.cs
@@ -1,12 +1,17 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using ToDoList.Models;
 using System.Collections.Generic;
+using System;
 
 namespace ToDoList.Tests
 {
   [TestClass]
-  public class ItemTest
+  public class ItemTest : IDisposable
   {
+    public void Dispose()
+    {
+      Item.ClearAll();
+    }
 
     [TestMethod]
     public void ItemConstructor_CreatesInstanceOfItem_Item()
@@ -49,32 +54,44 @@ namespace ToDoList.Tests
       Assert.AreEqual(updatedDescription, result);
     }
 
+
     [TestMethod]
     public void GetAll_ReturnsEmptyList_ItemList()
     {
-      // Arrange
+      //Arrange
       List<Item> newList = new List<Item> { };
 
-      // Act
+      //Act
       List<Item> result = Item.GetAll();
 
-      // Assert
+      // foreach (Item thisItem in result)
+      // {
+        // Console.WriteLine("Output from empty list GetAll test: " + thisItem.GetDescription());
+      // }
+
+      //Assert
       CollectionAssert.AreEqual(newList, result);
     }
 
     [TestMethod]
     public void GetAll_ReturnsItems_ItemList()
     {
-      // Arrange
+      //Arrange
       string description01 = "Walk the dog";
       string description02 = "Wash the dishes";
       Item newItem1 = new Item(description01);
       Item newItem2 = new Item(description02);
-
-      // act
       List<Item> newList = new List<Item> { newItem1, newItem2 };
 
-      //assert
+      //Act
+      List<Item> result = Item.GetAll();
+
+      // foreach (Item thisItem in result)
+      // {
+        // Console.WriteLine("Output from second GetAll test: " + thisItem.GetDescription());
+      // }
+
+      //Assert
       CollectionAssert.AreEqual(newList, result);
     }
 
